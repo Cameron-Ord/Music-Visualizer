44f0754f3e3a1af9828369394c4374926c9b5575
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2024-10-01 10:38:12 -0600

    start up with window border by default

--- src/window.cpp
+++ src/window.cpp
@@ -4,7 +4,7 @@
 void *SDL2Window::create_window(SDL_Window **w) {
     *w = SDL_CreateWindow(
         "Music Visualizer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600,
-        400, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_BORDERLESS);
+        400, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     if (!*w) {
         fprintf(stderr, "Could not create window! -> %s\n", SDL_GetError());
         return NULL;
